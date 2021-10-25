7e1879f34338689a818fc832b49340a8ba9bfc20
fondorn75 <zatoichi.cadzicu@gmail.com>
2021-10-25 18:37:06 +0300

    add project 003

---
+++ examples003/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace examples003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = 5;
+            int numberB = 6;
+            int result = numberA + numberB;
+            Console.WriteLine(result);
+        }
+    }
+}
