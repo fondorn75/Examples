1128fdd604166b5032eecc155db45f76bdffe1a0
fondorn75 <zatoichi.cadzicu@gmail.com>
2021-10-25 18:45:17 +0300

    add project 004 new

--- Examples004/Program.cs
+++ Examples004/Program.cs
@@ -6,7 +6,9 @@ namespace Examples004
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double numberA = 12;
+            double numberB = 5;
+            Console.WriteLine(numberA / numberB);
         }
     }
 }
