078f71c68140d69fe0c4729ff490efae9c53811f
fondorn75 <zatoichi.cadzicu@gmail.com>
2021-10-29 19:57:03 +0300

    examles 008 009

---
+++ Examples008_IntroMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 12;
+int b1 = 2;
+int c1 = 8;
+int a2 = 11;
+int b2 = 423;
+int c2 = 22;
+int a3 = 24;
+int b3 = 15;
+int c3 = 6;
+
+int max = Max(Max(a1, b1, c1), 
+              Max(a2, b2, c2), 
+              Max(a3, b3, c3));
+
+Console.WriteLine(max);
