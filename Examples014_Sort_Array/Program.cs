4d27076faded11097238e08e47eb5f7d9cec57fb
fondorn75 <zatoichi.cadzicu@gmail.com>
2021-11-04 21:08:09 +0300

    add examples 014 Sort array

---
+++ Examples014_Sort_Array/Program.cs
@@ -0,0 +1,52 @@
+﻿// Сортировка массива.
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void printArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSortMax(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+void SelectionSortMin(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+printArray(arr);
+SelectionSortMax(arr);
+printArray(arr);
+SelectionSortMin(arr);
+printArray(arr);
