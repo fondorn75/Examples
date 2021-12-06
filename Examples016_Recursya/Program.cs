398594a4038d8f25088388dda00888f42ce47a6e
Добрин Дмитрий <zatoichi.cadzicu@gmail.com>
2021-12-06 19:02:34 +0300

    modify Examples016_recursiya

--- Examples016_Recursya/Program.cs
+++ Examples016_Recursya/Program.cs
@@ -52,11 +52,29 @@
 // CatalogInfo(path);
 
 
-void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+// void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+// {
+//     if (count > 1) Towers(with, some, on, count - 1);
+//     Console.WriteLine($"{with} >> {on}");
+//     if (count > 1) Towers(some, on, with, count - 1);
+// }
+
+// Towers();
+
+string emp = string.Empty;
+
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+
+void InOrderTraversal(int pos = 1)
 {
-    if (count > 1) Towers(with, some, on, count - 1);
-    Console.WriteLine($"{with} >> {on}");
-    if (count > 1) Towers(some, on, with, count - 1);
+    if (pos < tree.Length)
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        if (left < tree.Length && !string.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+        Console.WriteLine(tree[pos]);
+        if (right < tree.Length && !string.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+    }
 }
 
-Towers();
+InOrderTraversal();
